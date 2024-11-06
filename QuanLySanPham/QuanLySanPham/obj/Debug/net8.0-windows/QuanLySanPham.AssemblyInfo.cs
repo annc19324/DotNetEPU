26b7c70f86a64845bb9ded430d93046597ae1338
annc19324 <annc19324@gmail.com>
2024-11-07 00:46:26 +0700

    update

--- QuanLySanPham/QuanLySanPham/obj/Debug/net8.0-windows/QuanLySanPham.AssemblyInfo.cs
+++ QuanLySanPham/QuanLySanPham/obj/Debug/net8.0-windows/QuanLySanPham.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLySanPham")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435ca3f1c3bfb8fa5b771c398897f58828596b54")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLySanPham")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLySanPham")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
