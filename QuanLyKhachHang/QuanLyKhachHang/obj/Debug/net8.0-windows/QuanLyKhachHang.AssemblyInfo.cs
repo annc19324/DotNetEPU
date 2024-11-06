dd735e07b204c63310902e40cd33dd23a35415df
annc19324 <annc19324@gmail.com>
2024-11-06 22:31:00 +0700

    update

--- QuanLyKhachHang/QuanLyKhachHang/obj/Debug/net8.0-windows/QuanLyKhachHang.AssemblyInfo.cs
+++ QuanLyKhachHang/QuanLyKhachHang/obj/Debug/net8.0-windows/QuanLyKhachHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22211fe579b421062bf576aeb797ed9c42ffac05")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
